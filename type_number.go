833ed3b9da730e6278e295ff7bfb3b6e318d2949
valentinHenry <valentin.hnry@gmail.com>
2022-07-21 23:36:17 +0200

    feat: add TNumbers from 0 to 10

--- type_number.go
+++ type_number.go
@@ -16,6 +16,42 @@ type N1[N Number] struct{}
 
 func (i N1[N]) Of() N { return 1 }
 
+type N2[N Number] struct{}
+
+func (i N2[N]) Of() N { return 2 }
+
+type N3[N Number] struct{}
+
+func (i N3[N]) Of() N { return 3 }
+
+type N4[N Number] struct{}
+
+func (i N4[N]) Of() N { return 4 }
+
+type N5[N Number] struct{}
+
+func (i N5[N]) Of() N { return 5 }
+
+type N6[N Number] struct{}
+
+func (i N6[N]) Of() N { return 6 }
+
+type N7[N Number] struct{}
+
+func (i N7[N]) Of() N { return 7 }
+
+type N8[N Number] struct{}
+
+func (i N8[N]) Of() N { return 8 }
+
+type N9[N Number] struct{}
+
+func (i N9[N]) Of() N { return 9 }
+
+type N10[N Number] struct{}
+
+func (i N10[N]) Of() N { return 10 }
+
 // Create the one you need
 
 func applyOn[N Number, P Predicate](left N, right any, fn func(N, N) bool, p P) (bool, error) {
